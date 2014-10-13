c017d5fc0dca94010d5d4081ec8f46b50aff78fd
cdepman <cdepman@gmail.com>
2014-10-12 18:40:40 -0700

    stringifyJSON and getElementsByClassName both passing all tests, may need refactoring

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,20 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+	var elementArray = [];
+	var nodeSurfer = function(currentNode){
+		if (currentNode.classList != undefined){
+			for (var i = 0; i < currentNode.classList.length; i++){
+				if (currentNode.classList[i] === className){
+					elementArray.push(currentNode);
+				}
+			}
+		}
+		for (var i = 0; i < currentNode.childNodes.length; i++){
+			nodeSurfer(currentNode.childNodes[i]);
+		}
+	}
+	nodeSurfer(document.body);
+	return elementArray;
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,56 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
+// exercise: refactor with sub-methods!
+
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	//case: null
+	if (obj === null) {
+		return "null";
+	}
+	//case: undefined
+	if (obj === undefined){
+		return 'undefined';
+	}
+	//case: object
+	if (obj instanceof Object){
+		if (obj instanceof Array){
+			if (obj.length === 0){
+				return '[]';
+			}
+			var result = '';
+			for (var i = 0; i < obj.length; i++){
+				if (i === obj.length - 1){
+					result += stringifyJSON(obj[i]);
+				}
+				else {
+					result += stringifyJSON(obj[i]) + ',';
+				}
+			}
+			return '[' + result + ']';
+		}
+		else if (Object.getOwnPropertyNames(obj).length === 0){
+			return '{}';
+		}
+		var result = '';
+		for (var key in obj){
+			if (obj[key] instanceof Function || obj[key] === undefined){
+				result += '';
+			}
+			else {
+				result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+			}
+		}
+		result = result.slice(0,-1);
+		return '{' + result + '}';
+	}
+	//case:primitive
+	else {
+		var objType = typeof(obj);
+		if (objType === "number" || objType === "boolean"){
+			return obj.toString();
+		}
+		return '"' + obj.toString() + '"';
+	}
 };
